212c06084f58d2153cd01419b3f461220491337a
inggu96 <dldlsrnr123@naver.com>
2023-06-14 20:27:18 +0900

    feat. title완성

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,13 +1,8 @@
 import { Outlet } from "react-router-dom";
 import styles from "./layout.module.scss";
-import Sidebar from "./Sidebar";
-
 const Layout = () => {
   return (
     <div className={styles.wrap}>
-      <div className={styles.sidebar}>
-        <Sidebar />
-      </div>
       <div className={styles.content}>
         <Outlet />
       </div>
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,45 @@
 import React from "react";
+import styles from "./home.module.scss";
 
 const Home = () => {
-  return <div>home</div>;
+  return (
+    <main className={styles.mainWrap}>
+      <div className={styles.titleWrap}>
+        <p>성장하는 개발자 이인국입니다</p>
+        <div className={styles.titleContent}>
+          <h1>이인국 | Portfolio</h1>
+        </div>
+        <div className={styles.introWrap}>
+          <div className={styles.introContent}>
+            <span>introductuon</span>
+            <div>Name : 이인국</div>
+            <div>Birth Date : 1996/08/24</div>
+            <div>Education : 영진전문대학 항공정비과 졸업</div>
+          </div>
+          <div className={styles.introContent}>
+            <span>Contact</span>
+            <div>Phone : 010-3686-4232</div>
+            <div>Email : dlsrnr403@gmail.com</div>
+            <div>gitHub : https://github.com/inggu96</div>
+          </div>
+          <div className={styles.introContent}>
+            <span>skill</span>
+            <div>Language : JavaScript</div>
+            <div>FrameWork / Library : React , Redux , Mui</div>
+            <div>Markup : HTML , CSS , SCSS , Emotion</div>
+            <div>Tool : Git , GitHub</div>
+          </div>
+          <div className={styles.eduWrap}></div>
+        </div>
+      </div>
+      <div className={styles.projectWrap}>
+        <div>
+          코드스테이츠 팀프로젝트 : 무비셀렉터 제작기간 : 2023.03.13 -
+          2023.04.14
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default Home;
